1f9b00b4c8cb1a0fd1a5926a40e71325adeafcaa
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2023-12-30 16:39:45 +0530

    react-router added

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivatePage from "./PrivateRoute";
+import About from "./AboutPage";
+
+export {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Cart,
+  Error,
+  Checkout,
+  PrivatePage,
+};
